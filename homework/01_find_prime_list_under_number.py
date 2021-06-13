632389d0bf8eebb0bc62cd853d5df3e3a5b692d9
bebest90 <1990devops@gmail.com>
2021-06-13 11:53:40 +0900

    100mb no

--- homework/01_find_prime_list_under_number.py
+++ homework/01_find_prime_list_under_number.py
@@ -2,8 +2,15 @@ input = 20
 
 
 def find_prime_list_under_number(number):
-    # 이 부분을 채워보세요!
-    return []
+    prime_list = []
+    for n in range(2, number + 1):
+        for i in range(2, n):
+            if n % i == 0:
+                break
+        else:
+            prime_list.append(n)
+
+    return prime_list
 
 
 result = find_prime_list_under_number(input)
